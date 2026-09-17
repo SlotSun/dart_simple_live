88f383cfd8cd6b5876ee2ee3065163672713d1ba
SlotSun <slot_sun@outlook.com>
2026-09-18 01:06:17 +0800

    fix(window): 空安全导致的bug

--- simple_live_app/lib/modules/live_room/player/player_controller.dart
+++ simple_live_app/lib/modules/live_room/player/player_controller.dart
@@ -339,10 +339,14 @@ mixin PlayerSystemMixin on PlayerMixin, PlayerStateMixin, PlayerDanmakuMixin {
       setPortraitOrientation();
     } else {
       // 退回原来的大小
-      if(windowMaxState.value) await windowManager.maximize();
-      if(_lastWindowSize != null) await windowManager.setSize(_lastWindowSize!);
-      if(_lastWindowPosition != null) await windowManager.setPosition(_lastWindowPosition!);
-      Log.d('last_window_size:${_lastWindowSize!.width}__${_lastWindowSize!.height}');
+      if (windowMaxState.value) await windowManager.maximize();
+      if (_lastWindowSize != null) {
+        await windowManager.setSize(_lastWindowSize!);
+      }
+      if (_lastWindowPosition != null) {
+        await windowManager.setPosition(_lastWindowPosition!);
+      }
+      Log.d('last_window_size:${_lastWindowSize?.width}__${_lastWindowSize?.height}');
       Log.d('last_window_position:${_lastWindowPosition?.dx}__${_lastWindowPosition?.dy}');
       windowManager.setFullScreen(false);
       windowManager.setTitleBarStyle(TitleBarStyle.normal);
